b2f763b301cde8bb5bfe5680b9c5276a40474d50
Cryowatt <Cryowatt@users.noreply.github.com>
2022-08-28 18:44:47 -0700

    Adding mapper param to CPU Setting the correct reset vector

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,4 +1,6 @@
-use std::collections::VecDeque;
+use std::{collections::VecDeque};
+
+use crate::roms::Mapper;
 
 type MicrocodeTask = fn(&mut Mos6502);
 
@@ -17,7 +19,7 @@ impl Mos6502 {
     pub fn new() -> Self {
         Self {
             a: 0x00,
-            pc: 0x0000,
+            pc: 0xfffc,
             p: 0x34,
             s: 0xFD,
             x: 0x00,
@@ -31,24 +33,31 @@ impl Mos6502 {
 pub trait RP2A03 {
     fn zero_page_indexed();
 
-    fn cycle(self: &mut Self);
+    fn cycle(self: &mut Self, mapper: &mut dyn Mapper);
 
-    fn decode_opcode(self: &mut Self) -> MicrocodeTask;
+    fn decode_opcode(self: &mut Self, mapper: &mut dyn Mapper) -> MicrocodeTask;
 }
 
 impl RP2A03 for Mos6502 {
     fn zero_page_indexed() {}
 
-    fn cycle(self: &mut Self) {
+    fn cycle(self: &mut Self, mapper: &mut dyn Mapper) {
         let microcode = match self.cycle_microcode_queue.pop_front() {
             Some(microcode) => microcode,
-            None => self.decode_opcode()
+            None => self.decode_opcode(mapper)
         };
         
         microcode(self);
     }
 
-    fn decode_opcode(self: &mut Self) -> MicrocodeTask {
-        todo!();
+    fn decode_opcode(self: &mut Self, mapper: &mut dyn Mapper) -> MicrocodeTask {
+        let opcode = mapper.read(self.pc);
+        self.pc += 1;
+
+        match opcode {
+            _ => panic!("Unsupported opcode {}", opcode),
+        }
+        
+        //todo!();
     }
 }
